610728b9c11719b6b77abf0614ad99495cea69dd
Kotet <kotet0418@gmail.com>
2019-02-03 12:47:07 +0900

    adding more spheres to the scene; rudimentary materials

--- tinyraytracer.d
+++ tinyraytracer.d
@@ -7,6 +7,8 @@ enum height = 768;
 enum filename = "./out.ppm";
 
 enum fov = PI / 2.0; // 90
+enum far = 1000.0f;
+
 enum fovtan = tan(fov / 2.0);
 enum aspect_ratio = width / cast(float) height;
 
@@ -14,7 +16,7 @@ alias vec3f = float[3];
 
 vec3f normalize(vec3f v)
 {
-    vec3f w = v[] / sqrt(dotp(v,v));
+    vec3f w = v[] / sqrt(dotp(v, v));
     return w;
 }
 
@@ -26,14 +28,25 @@ float dotp(vec3f lhs, vec3f rhs)
     return sum;
 }
 
+class Material
+{
+    vec3f diffuse_color;
+    this(vec3f color)
+    {
+        this.diffuse_color = color;
+    }
+}
+
 class Sphere
 {
     vec3f center;
     float radius;
-    this(vec3f center, float radius)
+    Material material;
+    this(vec3f center, float radius, Material material)
     {
         this.center = center;
         this.radius = radius;
+        this.material = material;
     }
 
     bool isIntersectWithTheRay(vec3f orig, vec3f dir, ref float dist)
@@ -53,15 +66,41 @@ class Sphere
     }
 }
 
-vec3f castRay(vec3f orig, vec3f dir, Sphere sphere)
+/**
+    hit:        ヒット座標
+    normal:     ヒットした面の法線ベクトル
+    material:   ヒットした面のマテリアル
+*/
+bool intersectScene(vec3f orig, vec3f dir, Sphere[] spheres, ref vec3f hit,
+        ref vec3f normal, ref Material material)
 {
-    float sphere_dist = float.max;
-    if (sphere.isIntersectWithTheRay(orig, dir, sphere_dist))
-        return [0.4, 0.4, 0.3];
+    float spheres_dist = float.max;
+    foreach (sphere; spheres)
+    {
+        float dist_i;
+        bool b = sphere.isIntersectWithTheRay(orig, dir, dist_i);
+        if (b && dist_i < spheres_dist)
+        {
+            spheres_dist = dist_i;
+            hit = orig[] + dir[] * dist_i;
+            normal = hit[] - sphere.center[];
+            normal.normalize();
+            material = sphere.material;
+        }
+    }
+    return spheres_dist < far;
+}
+
+vec3f castRay(vec3f orig, vec3f dir, Sphere[] spheres)
+{
+    vec3f hit, normal;
+    Material material;
+    if (intersectScene(orig, dir, spheres, hit, normal, material))
+        return material.diffuse_color;
     return [0.2, 0.7, 0.8];
 }
 
-void render(Sphere sphere)
+void render(Sphere[] spheres)
 {
     auto framebuffer = new vec3f[](width * height);
 
@@ -71,10 +110,10 @@ void render(Sphere sphere)
             float x = (2 * (i + 0.5) / cast(float) width - 1) * fovtan * aspect_ratio;
             float y = -(2 * (j + 0.5) / cast(float) height - 1) * fovtan;
             vec3f dir = [x, y, -1].normalize;
-            framebuffer[i + j * width] = castRay([0, 0, 0], dir, sphere);
+            framebuffer[i + j * width] = castRay([0, 0, 0], dir, spheres);
         }
 
-    auto ofile = File(filename, "w"); // write, binary
+    auto ofile = File(filename, "w"); // write
     ofile.writeln("P6");
     ofile.writeln(width, " ", height);
     ofile.writeln(255);
@@ -88,6 +127,14 @@ void render(Sphere sphere)
 
 void main()
 {
-    auto sphere = new Sphere([-3.0, 0.0, -16.0], 2.0);
-    render(sphere);
+    Material ivory = new Material([0.4, 0.4, 0.3]);
+    Material red_rubber = new Material([0.3, 0.1, 0.1]);
+
+    Sphere[] spheres;
+    spheres ~= new Sphere([-3.0, 0.0, -16.0], 2.0, ivory);
+    spheres ~= new Sphere([-1.0, -1.5, -12.0], 2.0, red_rubber);
+    spheres ~= new Sphere([1.5, -0.5, -18.0], 3.0, red_rubber);
+    spheres ~= new Sphere([7.0, 5.0, -18.0], 4.0, ivory);
+
+    render(spheres);
 }
