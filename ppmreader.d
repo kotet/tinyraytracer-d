b37202cc9cfeb56cfed81184141ce0b7efef5d49
Kotet <kotet0418@gmail.com>
2019-02-07 18:46:57 +0900

    P3 -> P6

--- ppmreader.d
+++ ppmreader.d
@@ -1,6 +1,6 @@
 module ppmreader;
 
-import std.stdio : File;
+import std.stdio;
 import std.string : chomp;
 import std.array : split;
 import std.conv : to;
@@ -14,20 +14,25 @@ class Envmap
     this(string filename)
     {
         auto file = File(filename, "r");
-        file.readln(); // P3
+        file.readln(); // P6
         file.readln(); // comment
+        
         auto wh = file.readln().chomp().split(' ').to!(size_t[]);
         this.width = wh[0];
         this.height = wh[1];
         v = new vec3f[](width * width);
         v[] = [0.5, 0.5, 0.5];
         size_t x = file.readln().chomp().to!size_t;
+
+        auto buf = new ubyte[](width * height * 3);
+        file.rawRead(buf);
+        size_t j = 0;
         foreach (i; ((width - height) / 2) * width .. (((width - height) / 2) * width
                 + width * height))
         {
-            v[i][0] = (file.readln().chomp().to!size_t) / cast(float) x;
-            v[i][1] = (file.readln().chomp().to!size_t) / cast(float) x;
-            v[i][2] = (file.readln().chomp().to!size_t) / cast(float) x;
+            v[i][0] = buf[j++] / cast(float) x;
+            v[i][1] = buf[j++] / cast(float) x;
+            v[i][2] = buf[j++] / cast(float) x;
         }
         this.height = this.width;
     }
